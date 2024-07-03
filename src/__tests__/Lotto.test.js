56d244fcc76ad257144bd622135298aefcc2218f
seeyoujeong <chan3358@naver.com>
2024-07-03 22:15:23 +0900

    test: 로또 번호 유효성 검사 테스트 추가

--- src/__tests__/Lotto.test.js
+++ src/__tests__/Lotto.test.js
@@ -2,9 +2,43 @@ import { describe, test, expect } from "vitest";
 import { Lotto } from "../domain/index.js";
 
 describe("Lotto 클래스 테스트.", () => {
+  test.each([
+    { value: 1 },
+    { value: "1" },
+    { value: true },
+    { value: null },
+    { value: undefined },
+    { value: {} },
+  ])(
+    "로또 번호로 적합하지 않은 값($value)을 받으면 오류가 발생한다.",
+    ({ value }) => {
+      expect(() => new Lotto(value)).toThrowError(
+        "로또 번호로 적합하지 않은 값입니다."
+      );
+    }
+  );
+
+  test("로또 번호가 6개 초과이면 오류가 발생한다.", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 6, 7])).toThrowError(
+      "로또 번호는 6개여야 합니다."
+    );
+  });
+
+  test("로또 번호가 6개 미만이면 오류가 발생한다.", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5])).toThrowError(
+      "로또 번호는 6개여야 합니다."
+    );
+  });
+
+  test("로또 번호 중에 숫자가 아닌 값이 있으면 오류가 발생한다.", () => {
+    expect(() => new Lotto(["a", 1, 2, 3, 4, 5])).toThrowError(
+      "로또 번호 중에 적합하지 않은 값이 있습니다."
+    );
+  });
+
   test("로또 번호 중에 중복되는 번호가 있으면 오류가 발생한다.", () => {
     expect(() => new Lotto([1, 2, 3, 4, 5, 5])).toThrowError(
-      "중복되는 번호가 있습니다."
+      "중복되는 로또 번호가 있습니다."
     );
   });
 });
