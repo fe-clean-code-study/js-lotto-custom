5a7c226a02b148838c35bea6f5b0751fee79dd2e
seeyoujeong <chan3358@naver.com>
2024-07-03 22:15:06 +0900

    feat: 로또 번호 유효성 검사 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -10,8 +10,20 @@ class Lotto {
   }
 
   static #validateLottoNumbers(lottoNumbers) {
+    if (!Array.isArray(lottoNumbers)) {
+      throw new Error("로또 번호로 적합하지 않은 값입니다.");
+    }
+
+    if (lottoNumbers.length !== 6) {
+      throw new Error("로또 번호는 6개여야 합니다.");
+    }
+
+    if (lottoNumbers.some((number) => typeof number !== "number")) {
+      throw new Error("로또 번호 중에 적합하지 않은 값이 있습니다.");
+    }
+
     if (isDuplicated(lottoNumbers)) {
-      throw new Error("중복되는 번호가 있습니다.");
+      throw new Error("중복되는 로또 번호가 있습니다.");
     }
   }
 }
