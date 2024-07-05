4f786728a81a45a1b7fd95fe1bfb8591064b2af2
seeyoujeong <chan3358@naver.com>
2024-07-05 15:11:41 +0900

    feat: 구입 금액 입력 구현

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import { inputManager, outputManager } from "./service/index.js";
+import { LottoMachine } from "./domain/index.js";
+
+async function main() {
+  const lottoMachine = await inputManager.retryScan(
+    "> 구입 금액을 입력해 주세요. ",
+    (inputValue) => {
+      const numValue = Number(inputValue);
+
+      return new LottoMachine(numValue);
+    }
+  );
+
+  outputManager.print(`${lottoMachine.count}개 구매했습니다.`);
+  outputManager.printAll(lottoMachine.lottos, (lottos) => lottos.numbers);
 }
 
 main();
