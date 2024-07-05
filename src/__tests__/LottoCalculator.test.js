d4e99f2e3aaf02545b48e6d635c5aa39d1a4b406
seeyoujeong <chan3358@naver.com>
2024-07-05 11:22:27 +0900

    test: 금액 관련 부분 및 중복 코드 수정

--- src/__tests__/LottoCalculator.test.js
+++ src/__tests__/LottoCalculator.test.js
@@ -8,13 +8,14 @@ import {
 
 describe("LottoCalculator 클래스 테스트", () => {
   test("1등부터 5등까지 당첨된 수를 가져온다.", () => {
-    const lottoMachine = new LottoMachine(6000);
+    const { price, lottos } = new LottoMachine(6000);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachine.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("1");
     expect(winningCounts).toHaveProperty("2");
@@ -30,67 +31,75 @@ describe("LottoCalculator 클래스 테스트", () => {
   });
 
   test("로또 번호 중에 1등 당첨이 하나있다.", () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const lottoMachineMock = vi.fn().mockReturnValueOnce({ lottos: [lotto] })();
+    const { price, lottos } = createLottoMachineMock([1, 2, 3, 4, 5, 6]);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachineMock.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("1", 1);
   });
 
   test("로또 번호 중에 2등 당첨이 하나있다.", () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 7]);
-    const lottoMachineMock = vi.fn().mockReturnValueOnce({ lottos: [lotto] })();
+    const { price, lottos } = createLottoMachineMock([1, 2, 3, 4, 5, 7]);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachineMock.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("2", 1);
   });
 
   test("로또 번호 중에 3등 당첨이 하나있다.", () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 8]);
-    const lottoMachineMock = vi.fn().mockReturnValueOnce({ lottos: [lotto] })();
+    const { price, lottos } = createLottoMachineMock([1, 2, 3, 4, 5, 8]);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachineMock.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("3", 1);
   });
 
   test("로또 번호 중에 4등 당첨이 하나있다.", () => {
-    const lotto = new Lotto([1, 2, 3, 4, 8, 9]);
-    const lottoMachineMock = vi.fn().mockReturnValueOnce({ lottos: [lotto] })();
+    const { price, lottos } = createLottoMachineMock([1, 2, 3, 4, 8, 9]);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachineMock.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("4", 1);
   });
 
   test("로또 번호 중에 5등 당첨이 하나있다.", () => {
-    const lotto = new Lotto([1, 2, 3, 8, 9, 10]);
-    const lottoMachineMock = vi.fn().mockReturnValueOnce({ lottos: [lotto] })();
+    const { price, lottos } = createLottoMachineMock([1, 2, 3, 8, 9, 10]);
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
 
-    const { winningCounts } = new LottoCalculator(
-      lottoMachineMock.lottos,
-      winningLotto
-    );
+    const { winningCounts } = new LottoCalculator({
+      price,
+      lottos,
+      winningLotto,
+    });
 
     expect(winningCounts).toHaveProperty("5", 1);
   });
 });
+
+function createLottoMachineMock(lottoNumbers) {
+  const lotto = new Lotto(lottoNumbers);
+  const lottoMachineMock = vi.fn();
+  lottoMachineMock.mockReturnValueOnce({ price: 1000, lottos: [lotto] });
+
+  return lottoMachineMock();
+}
