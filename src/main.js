efb7d6905cbba3e5c017a04ffc04d99a6b240234
jgjgill <dbdltm22@naver.com>
2024-07-07 20:00:14 +0900

    feat: main 구성

--- src/main.js
+++ src/main.js
@@ -1,5 +1,53 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import Lotto from "./domain/lotto.js";
+import { LottoView } from "./view/lotto.js";
+
+async function main() {
+  const winRuleSchema = {
+    1: { match: { count: 6 }, amount: 2_000_000_000 },
+    2: { match: { count: 5, bonus: true }, amount: 30_000_000 },
+    3: { match: { count: 5, bonus: false }, amount: 1_500_000 },
+    4: { match: { count: 4 }, amount: 50_000 },
+    5: { match: { count: 3 }, amount: 5_000 },
+  };
+
+  const {
+    purchaseInput,
+    winNumberInput,
+    bonusNumberInput,
+    lottoCountConsole,
+    lottoListConsole,
+    lottoResultConsole,
+    rateOfReturnConole,
+  } = LottoView();
+
+  const {
+    makeLottoCount,
+    makeLottoList,
+    makeCheckSchema,
+    makeLottoResult,
+    makeWinAmount,
+    makeRateOfReturn,
+  } = Lotto(winRuleSchema);
+
+  const money = await purchaseInput();
+  const lottoCount = makeLottoCount(money);
+  const lottoList = makeLottoList(lottoCount);
+
+  lottoCountConsole(lottoCount);
+  lottoListConsole(lottoList);
+
+  const winNumberList = await winNumberInput();
+  const bonusNumber = await bonusNumberInput();
+
+  const checkSchema = makeCheckSchema(winNumberList, bonusNumber);
+  const lottoResult = makeLottoResult(lottoList, checkSchema);
+
+  lottoResultConsole(winRuleSchema, lottoResult);
+
+  const winAmount = makeWinAmount(lottoResult);
+  const rateOfReturn = makeRateOfReturn(winAmount, money);
+
+  rateOfReturnConole(rateOfReturn);
 }
 
 main();
