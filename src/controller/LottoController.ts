384f27eb648943ddc7bf1dd294dc60ea13d4f94c
HongGunWoo <foxrain.gg@gmail.com>
2024-07-09 01:18:01 +0900

    feat: controller 구현

---
+++ src/controller/LottoController.ts
@@ -0,0 +1,65 @@
+import { LottoView } from '../View/LottoView.js';
+import { LottoGame } from '../model/LottoGame.js';
+
+export class LottoController {
+  private view: LottoView;
+  private lottoGame = new LottoGame();
+
+  constructor(view: LottoView) {
+    this.view = view;
+  }
+
+  public setLottoRule() {
+    // TODO 로또 타입을 입력 받는 기능을 추가로 구현해야함.
+    const lottoType = '1';
+    const lottoPrice = 1000;
+
+    this.lottoGame.setLottoRule(lottoPrice, lottoType);
+  }
+
+  public async purchaseTickets() {
+    const receivedMoney = await this.view.getMoney();
+    this.lottoGame.setTickets(receivedMoney);
+  }
+
+  public displayTickets() {
+    const tickets = this.lottoGame.getTickets();
+
+    this.view.printMessage(`${tickets.length}개를 구매했습니다.`);
+    tickets.forEach((ticket) => {
+      this.view.printMessage(`[${ticket.join(', ')}]`);
+    });
+    this.view.printMessage('\n');
+  }
+
+  public async setWinningNumbers() {
+    const { numbers, bonusNumber } = await this.getWinningLotto();
+    this.lottoGame.setWinningLotto(numbers, bonusNumber);
+  }
+
+  public displayResult() {
+    this.view.printMessage('당첨 통계');
+    this.view.printMessage('--------------------');
+
+    const result = this.lottoGame.getResult();
+
+    result.formattedResult.forEach(
+      ({ count, includeBonus, matchCount, reward }) => {
+        this.view.printMessage(
+          `${matchCount}개 일치${
+            includeBonus ? ', 보너스 볼 일치' : ''
+          } (${reward}원) - ${count}개`
+        );
+      }
+    );
+
+    this.view.printMessage(`총 수익률은 ${result.totalRewardPercent}% 입니다.`);
+  }
+
+  private async getWinningLotto() {
+    const numbers = await this.view.getWinningNumbers();
+    const bonusNumber = await this.view.getWinningBonusNumber();
+
+    return { numbers, bonusNumber };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import { LottoController } from './controller/LottoController.js';
+import { LottoView } from './View/LottoView.js';
+
+async function main() {
+  const view = new LottoView();
+  const controller = new LottoController(view);
+  controller.setLottoRule();
+  await controller.purchaseTickets();
+  controller.displayTickets();
+  await controller.setWinningNumbers();
+  controller.displayResult();
 }
 
-main();
+await main();
