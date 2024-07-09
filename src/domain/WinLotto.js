28bd6f29dab3e2d9a2c3f304840407404cbe8b9c
Suyeon <msjgr2801@gmail.com>
2024-07-09 13:52:59 +0900

    feat: WinLotto 클래스 작성

---
+++ src/domain/WinLotto.js
@@ -0,0 +1,78 @@
+import Lotto from './Lotto.js';
+import LottoRule from './LottoRule.js';
+import { accord } from '../utils/Array.js';
+
+export default class WinLotto extends Lotto {
+  _winLotto;
+
+  constructor(lotto) {
+    super(lotto);
+    this._winLotto = undefined;
+    this.set(lotto);
+  }
+
+  static get DEFAULT_LENGTH() {
+    return 7;
+  }
+
+  set(lotto) {
+    const lottoError = WinLotto.getLottoError(lotto);
+
+    if (lottoError !== undefined) {
+      throw new Error(lottoError);
+    }
+
+    this._winLotto = lotto;
+  }
+
+  get() {
+    return [...this._winLotto];
+  }
+
+  checkRank(compareLotto) {
+    const compareLottoError = Lotto.getLottoError(compareLotto);
+    const winLottoError = WinLotto.getLottoError(this._winLotto);
+
+    if (compareLottoError !== undefined || winLottoError !== undefined) {
+      return compareLottoError || winLottoError;
+    }
+
+    const accordCount = accord(
+      this._winLotto.slice(0, LottoRule.DEFAULT_LENGTH),
+      compareLotto,
+    );
+    const isBonusCorrect = compareLotto.includes(this._winLotto.at(-1));
+
+    return isBonusCorrect
+      ? WinLotto.RANK_BY_ACCORD[accordCount].bonus
+      : WinLotto.RANK_BY_ACCORD[accordCount].base;
+  }
+
+  static getLottoError(lotto) {
+    const commonError = Lotto.getCommonLottoError(lotto);
+
+    if (commonError !== undefined) {
+      return commonError;
+    }
+
+    const lottoSet = new Set(lotto);
+
+    if (lottoSet.size !== WinLotto.DEFAULT_LENGTH) {
+      return `당첨 로또 번호의 개수는 ${WinLotto.DEFAULT_LENGTH}개 여야 합니다.`;
+    }
+
+    return undefined;
+  }
+
+  static getBonusNumberError(bonusNumber) {
+    if (typeof bonusNumber !== 'number') {
+      return 'bonusNumber 가 숫자가 아닙니다.';
+    }
+
+    if (bonusNumber < Lotto.MIN_NUMBER || Lotto.MAX_NUMBER < bonusNumber) {
+      return 'bonusNumber 는 1 ~ 45 사이여야 합니다.';
+    }
+
+    return undefined;
+  }
+}
