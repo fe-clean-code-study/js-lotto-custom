a49de74abfcf7e6d5ad0b50f60f0c5eb53f1e079
Suyeon <msjgr2801@gmail.com>
2024-07-09 13:52:31 +0900

    feat: Lotto 클래스 작성

---
+++ src/domain/Lotto.js
@@ -0,0 +1,35 @@
+export default class Lotto {
+  _lotto;
+
+  constructor(lotto) {
+    this.set(lotto);
+  }
+
+  set(lotto) {
+    const lottoError = Lotto.getLottoError(lotto);
+
+    if (lottoError !== undefined) {
+      return lottoError;
+    }
+
+    this._lotto = lotto;
+  }
+
+  get() {
+    return [...this._lotto];
+  }
+
+  static getLottoError(lotto) {
+    const commonError = Lotto.getCommonLottoError(lotto);
+
+    if (commonError !== undefined) return commonError;
+
+    const lottoSet = new Set(lotto);
+
+    if (lottoSet.size !== Lotto.DEFAULT_LENGTH) {
+      return `일반 로또 번호의 개수는 ${Lotto.DEFAULT_LENGTH}개 여야 합니다.`;
+    }
+
+    return undefined;
+  }
+}
