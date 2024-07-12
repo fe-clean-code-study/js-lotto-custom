db61a5698dae15222579693fcba267c2d94682a7
Suyeon <msjgr2801@gmail.com>
2024-07-12 13:40:28 +0900

    feat: validator 함수 사용시 타입:값 형태로 사용할 수 있도록 하기

--- src/validator/createValidator.js
+++ src/validator/createValidator.js
@@ -1,31 +1,24 @@
 const createValidator = (validation) => {
-  const validateKeyValue = (key, value) => {
-    if (validation[key] === undefined) {
-      throw new Error('해당 키 값이 validation 내부에 존재하지 않습니다.');
-    }
-    const validate = validation[key](value);
+  const validateKeyValue = (keyType, value) => {
+    Object.keys(validation[keyType]).forEach((validationKey) => {
+      const validate = validation[keyType][validationKey](value);
 
-    if (validate !== true) {
-      throw new Error(validate);
-    }
+      if (validate !== true) {
+        throw new Error(validate);
+      }
+    });
   };
 
-  return (keys, values) => {
-    if (typeof keys === 'string' && typeof values === 'string') {
-      validateKeyValue(keys, values);
-      return;
-    }
-    if (Array.isArray(keys) && Array.isArray(values)) {
-      if (keys.length !== values.length) {
-        throw new Error('키 배열의 개수와 값 배열의 개수가 일치해야 합니다.');
+  return (values) => {
+    Object.entries(values).map(([keyType, value]) => {
+      if (validation[keyType] === undefined) {
+        throw new Error(
+          `${keyType} 가 초기화 시 전달한 validation 내부에 존재하지 않습니다.`,
+        );
       }
-      keys.forEach((key, index) => validateKeyValue(key, values[index]));
-      return;
-    }
 
-    throw new Error(
-      '매개변수로 전달한 keys 와 values 의 타입은 동일해야 합니다.',
-    );
+      validateKeyValue(keyType, value);
+    });
   };
 };
 
