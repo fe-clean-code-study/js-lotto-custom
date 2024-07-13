368e2daa9e77727b9e8c0fdf9b6fe3e3db445e84
Suyeon <msjgr2801@gmail.com>
2024-07-13 15:31:59 +0900

    refactoring: validator 프로퍼티 일관성 있게 변경

--- src/validator/View/InputValidator.js
+++ src/validator/View/InputValidator.js
@@ -1,27 +1,27 @@
 const InputValidator = {
   message: {
-    messageType: (value) =>
+    checkType: (value) =>
       typeof value === 'string' || 'message 는 문자열 타입이어야 합니다.',
-    messageLength: (value) =>
+    checkLength: (value) =>
       value.trim().length >= 1 || 'message 는 1글자 이상이어야 합니다',
   },
   options: {
-    optionsType: (value) =>
+    checkType: (value) =>
       typeof value === 'object' || 'options 는 개체 타입이어야 합니다.',
-    optionsHasPostProcessFn: (value) =>
+    checkHasPostProcessFn: (value) =>
       value.hasProperty('postProcessFn') ||
       'options 는 postProcessFn 프로퍼티를 가지고 있어야 합니다.',
-    optionsHasValidation: (value) =>
+    checkHasValidation: (value) =>
       value.hasProperty('validation') ||
       'options 는 validation 프로퍼티를 가지고 있어야 합니다.',
   },
   postProcessFn: {
-    PostPrcoessFnType: (value) =>
+    checkType: (value) =>
       (value !== undefined && typeof value !== 'function') ||
       'postProcessFn 는 함수여야 합니다.',
   },
   validate: {
-    validateType: (value) =>
+    checkType: (value) =>
       (value !== undefined && typeof value !== 'function') ||
       'validate 는 함수여야 합니다.',
   },
--- src/validator/View/OutputValidator.js
+++ src/validator/View/OutputValidator.js
@@ -1,10 +1,10 @@
 const OutputValidator = {
   templateKey: {
-    templateKeyType: (value) =>
+    checkType: (value) =>
       typeof value === 'string' || 'templateKey 의 타입이 문자열이 아닙니다.',
   },
   templateVariables: {
-    templateVariablesType: (value) =>
+    checkType: (value) =>
       typeof value === 'object' ||
       'templateVariables 의 타입이 객체가 아닙니다.',
   },
