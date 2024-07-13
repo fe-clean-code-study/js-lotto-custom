046974b94b1e9ecc0af34a050022695cd8b48d35
seeyoujeong <chan3358@naver.com>
2024-07-13 11:39:42 +0900

    feat: LottoNumber 클래스 추가

---
+++ src/model/LottoNumber.js
@@ -0,0 +1,34 @@
+import { throwErrorWithCondition, validate } from "../utils/index.js";
+
+class LottoNumber {
+  static #MIN_NUMBER = 1;
+  static #MAX_NUMBER = 45;
+
+  #number;
+
+  constructor(number) {
+    LottoNumber.#validateNumber(number);
+
+    this.#number = number;
+  }
+
+  get value() {
+    return this.#number;
+  }
+
+  static #validateNumber(number) {
+    validate.integer(number, "로또 번호로 적합하지 않은 값입니다.");
+
+    throwErrorWithCondition(
+      number < LottoNumber.#MIN_NUMBER,
+      `로또 번호는 ${LottoNumber.#MIN_NUMBER}보다 커야 합니다.`
+    );
+
+    throwErrorWithCondition(
+      LottoNumber.#MAX_NUMBER < number,
+      `로또 번호는 ${LottoNumber.#MAX_NUMBER}보다 작아야 합니다.`
+    );
+  }
+}
+
+export default LottoNumber;
--- src/model/index.js
+++ src/model/index.js
@@ -2,3 +2,4 @@ export { default as Lotto } from "./Lotto.js";
 export { default as LottoMachine } from "./LottoMachine.js";
 export { default as WinningLotto } from "./WinningLotto.js";
 export { default as LottoCalculator } from "./LottoCalculator.js";
+export { default as LottoNumber } from "./LottoNumber.js";
