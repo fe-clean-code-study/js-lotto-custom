85b4a72d1178bf4e7fd05b12c449d2499f7b2dc3
seeyoujeong <chan3358@naver.com>
2024-07-13 13:05:11 +0900

    test: 불필요한 테스트 삭제

--- src/__tests__/WinningLotto.test.js
+++ src/__tests__/WinningLotto.test.js
@@ -2,33 +2,6 @@ import { describe, expect, test } from "vitest";
 import { WinningLotto } from "../model/index.js";
 
 describe("WinningLotto 클래스 테스트", () => {
-  test("보너스 번호가 정수가 아니면 오류가 발생한다.", () => {
-    const lottoNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = "a";
-
-    expect(() => new WinningLotto(lottoNumbers, bonusNumber)).toThrowError(
-      "보너스 번호는 정수여야 합니다."
-    );
-  });
-
-  test("보너스 번호가 1보다 작으면 오류가 발생한다.", () => {
-    const lottoNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 0;
-
-    expect(() => new WinningLotto(lottoNumbers, bonusNumber)).toThrowError(
-      "보너스 번호는 1이상이어야 합니다."
-    );
-  });
-
-  test("보너스 번호가 45보다 크면 오류가 발생한다.", () => {
-    const lottoNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 46;
-
-    expect(() => new WinningLotto(lottoNumbers, bonusNumber)).toThrowError(
-      "보너스 번호는 45이하여야 합니다."
-    );
-  });
-
   test("당첨 번호 중에 보너스 번호와 중복되는 번호가 있으면 오류가 발생한다.", () => {
     const lottoNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 6;
