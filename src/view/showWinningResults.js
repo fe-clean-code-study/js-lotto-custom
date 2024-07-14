8fb164fb36e8241bfc39885122a7f1ecec4ef52e
seeyoujeong <chan3358@naver.com>
2024-07-14 17:28:42 +0900

    refactor: 로또 순위 정보를 바탕으로 결과를 보여주게 수정

--- src/view/showWinningResults.js
+++ src/view/showWinningResults.js
@@ -1,15 +1,22 @@
+import { LOTTO_RANKING_INFO } from "../constants/index.js";
 import { outputManager } from "../service/index.js";
+import { formatKoreanCurrency } from "../utils/index.js";
 
 const showWinningResults = (winningCounts, rateOfReturn) => {
   outputManager.print("당첨 통계");
   outputManager.print("--------------------");
-  outputManager.print(`3개 일치 (5,000원) - ${winningCounts[5]}개`);
-  outputManager.print(`4개 일치 (50,000원) - ${winningCounts[4]}개`);
-  outputManager.print(`5개 일치 (1,500,000원) - ${winningCounts[3]}개`);
-  outputManager.print(
-    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningCounts[2]}개`
+
+  LOTTO_RANKING_INFO.reverse().forEach(
+    ({ ranking, matchingCount, isBonusMatch, prizeMoney }) => {
+      const bonusMatchMessage = isBonusMatch ? `, 보너스 볼 일치` : "";
+      const formattedPrizeMoney = formatKoreanCurrency(prizeMoney);
+
+      outputManager.print(
+        `${matchingCount}개 일치${bonusMatchMessage} (${formattedPrizeMoney}원) - ${winningCounts[ranking]}개`
+      );
+    }
   );
-  outputManager.print(`6개 일치 (2,000,000,000원) - ${winningCounts[1]}개`);
+
   outputManager.print(`총 수익률은 ${rateOfReturn}%입니다.`);
 };
 
