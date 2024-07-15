940c02e00f7a843782437ed8e972c3906e9e5880
Suyeon <msjgr2801@gmail.com>
2024-07-15 15:35:08 +0900

    feat: LottoController 기능 구현

---
+++ src/domain/LottoController.js
@@ -0,0 +1,70 @@
+import createValidator from '../utils/createValidator.js';
+import InputValidator from '../validator/View/InputValidator.js';
+import LottoValidation from '../validator/domain/LottoValidator.js';
+import LottoRule from './LottoRule.js';
+
+export default class LottoController {
+  constructor({ view, lotto, winLotto }) {
+    this.view = new view();
+    this.lotto = lotto;
+    this.winLotto = winLotto;
+  }
+
+  async run() {
+    while (true) {
+      const lottos = await this.getLottos();
+      this.view.printLottos(lottos.map((lotto) => lotto.get()));
+
+      const winLotto = await this.getwinLotto();
+      const { prize, purchases, details, prizes } =
+        winLotto.getWinninResult(lottos);
+
+      this.view.printwinsResult({ ...details, prizes });
+      this.view.printRevenue(prize / purchases);
+
+      const restart = await this.restart();
+
+      if (restart === 'n' || restart === 'N') {
+        break;
+      }
+    }
+  }
+
+  async getLottos() {
+    const validator = createValidator(LottoValidation);
+    const money = await this.view.inputMoney({
+      postProcessFn: (value) => Number(value),
+      validate: validator({ money }),
+    });
+    const quantity = LottoRule.exchange(money);
+
+    return [...new Array(quantity)].map(
+      new this.lotto(LottoRule.generateLottoNumbers()),
+    );
+  }
+
+  async getwinLotto() {
+    const validator = createValidator(LottoValidation);
+    const winLotto = await this.view.inputwinLotto({
+      postProcessFn: (value) =>
+        value.split(',').map((number) => Number(number)),
+      validate: validator({ common: winLotto }),
+    });
+    const bonusNumber = await this.view.inputBonusNumber({
+      postProcessFn: (value) => Number(value),
+      validate: validator({ number: bonusNumber }),
+    });
+
+    return new this.winLotto([...winLotto, bonusNumber]);
+  }
+
+  async getRestart() {
+    const validator = createValidator(InputValidator);
+    const restart = await this.view.inputRestart({
+      postProcessFn: (value) => value.trim(),
+      validate: validator({ restart }),
+    });
+
+    return restart;
+  }
+}
