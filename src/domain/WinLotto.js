7dc05e4eff773500cdf293b0a2b984a45c03ef43
Suyeon <msjgr2801@gmail.com>
2024-07-15 15:34:42 +0900

    feat: WinLotto 클래스에서 로또 결과를 출력하는 기능 구현

--- src/domain/WinLotto.js
+++ src/domain/WinLotto.js
@@ -1,6 +1,7 @@
 import Lotto from './Lotto.js';
 import LottoValidator from '../validator/domain/LottoValidator.js';
 import createValidator from '../utils/createValidator.js';
+import LottoRule from './LottoRule.js';
 
 export default class WinLotto extends Lotto {
   constructor(...lotto) {
@@ -13,5 +14,48 @@ export default class WinLotto extends Lotto {
 
     validator({ common: normalizedLotto, winLotto: normalizedLotto });
     this.lotto = normalizedLotto;
+  }
+
+  isBonusCorrect(lotto) {
+    const bonusNumber = this.lotto.at(-1);
+
+    return lotto.includes(bonusNumber);
+  }
+
+  getRank(lotto) {
+    const bonusCorrect = this.isBonusCorrect(lotto);
+    const accordCount = this.accord(lotto) - bonusCorrect ? 1 : 0;
+
+    for (const rank of LottoRule.winningInfo) {
+      if (accordCount === LottoRule.winningInfo[rank].accord) {
+        if (LottoRule.winningInfo[rank].checkBonus && bonusCorrect) {
+          return rank;
+        }
+
+        return rank;
+      }
+    }
+  }
+
+  getWinningResult(lottos) {
+    const winningResult = {
+      prize: 0,
+      purchases: lottos.length * LottoRule.lottoPrice,
+      details: {},
+      prizes: {},
+    };
+
+    Object.entries(LottoRule.moneyByRank).forEach(([rank, money]) => {
+      winningResult.details[rank] = 0;
+      winningResult.prizes[rank + 'Money'] = money;
+    });
+    lottos.forEach((lotto) => {
+      const rank = this.getRank(lotto.get());
+
+      winningResult.details[rank] += 1;
+      winningResult.prize += LottoRule.moneyByRank[rank];
+    });
+
+    return winningResult;
   }
 }
