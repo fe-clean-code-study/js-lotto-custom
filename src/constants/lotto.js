42f46b7a7ab96b757c1cd746d5e92341e42eea59
seeyoujeong <chan3358@naver.com>
2024-07-15 23:00:18 +0900

    refactor: 파일명 수정

---
+++ src/constants/lotto.js
@@ -0,0 +1,34 @@
+export const LOTTO = {
+  NUMBERS_SIZE: 6,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  PRICE: 1000,
+  RANKING_INFO: [
+    {
+      ranking: 1,
+      matchingCount: 6,
+      prizeMoney: 2_000_000_000,
+    },
+    {
+      ranking: 2,
+      matchingCount: 5,
+      isBonusMatch: true,
+      prizeMoney: 30_000_000,
+    },
+    {
+      ranking: 3,
+      matchingCount: 5,
+      prizeMoney: 1_500_000,
+    },
+    {
+      ranking: 4,
+      matchingCount: 4,
+      prizeMoney: 50_000,
+    },
+    {
+      ranking: 5,
+      matchingCount: 3,
+      prizeMoney: 5_000,
+    },
+  ],
+};
